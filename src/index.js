8d20a4dcd3c565df075227affe6c124f6e4bed36
Artur Almeida <arturoalmeidafreire@gmail.com>
2022-09-30 11:05:36 -0300

    feat: adiciona função de depósito

--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@ import express from "express";
 import { prismaClient } from "./prismaClients.js";
 const app = express();
 
-
 const port = 3000;
 
 app.use(express.json());
+
+const userExists = async (req, res, next) => {
+    const { cpf } = req.headers;
+    try {
+        const data = await prismaClient.user.findFirst({
+            where: {
+                cpf
+            },
+            select: {
+                id: true
+            }
+        })
+        if (!data) return res.status(400).json({ error: "User not found" });
+        req.userId = data.id;
+    
+        next();
+    } catch {
+        return res.status(400).json({ data: error, has_error: true })
+    }
+}
 
 app.get("/", (req, res) => {
     res.json({ message: "Executando!" }).send();
@@ -13,7 +32,17 @@ app.get("/", (req, res) => {
 
 app.get("/accountsList", async (req, res) => {
     try {
-        const data = await prismaClient.user.findMany();
+        const data = await prismaClient.user.findMany({
+            include: {
+                statement: {
+                    select: {
+                        id: true,
+                        type: true,
+                        value: true
+                    }
+                }
+            }
+        });
         return res.status(200).json({ accounts: data });
     } catch (error) {
         return res.status(400).json({ data: error, has_error: true });
@@ -36,4 +65,22 @@ app.post("/createAccount", async (req, res) => {
     return res.status(201).send();
 });
 
-app.listen(port, () => console.log(`Executando na porta ${port}`))
+app.post("/deposit", userExists, async (req, res) => {
+    const body = req.body;
+    const userId = req.userId;
+
+    try {  
+        const data = await prismaClient.statement.create({ 
+            data: {
+                ...body,
+                userId
+            }
+         })
+    } catch (error) {
+        return res.status(400).json({ data: error, has_error: true });
+    }
+
+    return res.status(201).send();
+});
+
+app.listen(port, () => console.log(`Executando na porta ${port}`));
